2b47cf3113fcfa7075826b9f62ba2dad0901cf24
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-07-04 00:13:29 -0300

    Reformatting and setting keyboard layout to us international.

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -1,6 +1,9 @@
-# starting applications
-conky      &>/dev/null &
-pypanel    &>/dev/null
+# start applications
+conky &> /dev/null &
+pypanel &> /dev/null &
+
+# set keyboard layout to us international
+setxkbmap us -variant altgr-intl &> /dev/null &
 
 # re-enable ctrl-alt-backspace killing screen feature
-setxkbmap -option terminate:ctrl_alt_bksp &>/dev/null &
+setxkbmap -option terminate:ctrl_alt_bksp &> /dev/null &
