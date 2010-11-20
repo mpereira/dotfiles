f8e4fa01373f657b6ad8f29ad199b154ab051b8d
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-11-20 15:52:04 -0200

    Use /home/murilo instead of hard-coded home path.

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -1,7 +1,7 @@
 conky &> /dev/null &
 pypanel &> /dev/null &
 
-feh --bg-scale /home/murilo/.wallpaper &> /dev/null &
+feh --bg-scale $HOME/.wallpaper &> /dev/null &
 
 setxkbmap us -variant altgr-intl &> /dev/null &
 setxkbmap -option terminate:ctrl_alt_bksp &> /dev/null &
