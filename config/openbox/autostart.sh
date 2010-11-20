0957a3d802ba0057d2ddaed72899d61c722bf914
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-11-20 17:20:56 -0200

    Wait for openbox to start to run programs.

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -1,7 +1,10 @@
-conky &> /dev/null &
-pypanel &> /dev/null &
-
-feh --bg-scale $HOME/.wallpaper &> /dev/null &
-
 setxkbmap us -variant altgr-intl &> /dev/null &
 setxkbmap -option terminate:ctrl_alt_bksp &> /dev/null &
+
+sleep 1
+
+{
+  feh --bg-scale $HOME/.wallpaper &
+  pypanel &
+  conky &
+} &> /dev/null &
