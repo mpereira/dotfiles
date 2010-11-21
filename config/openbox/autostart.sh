1be4b2316adfa2aa306aac4c64fb5fd877ec12a2
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-11-20 23:43:04 -0200

    Check if wallpaper file exists and sleep less.

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -1,10 +1,11 @@
 setxkbmap us -variant altgr-intl &> /dev/null &
 setxkbmap -option terminate:ctrl_alt_bksp &> /dev/null &
 
-sleep 1
+sleep 0.5
+
+[[ -s "$HOME/.wallpaper" ]] && feh --bg-scale $HOME/.wallpaper &> /dev/null &
 
 {
-  feh --bg-scale $HOME/.wallpaper &
   pypanel &
   conky &
 } &> /dev/null &
