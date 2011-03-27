26c902bfbc2bc24cc89430e1b958a16d809662a1
Murilo Pereira <murilo@murilopereira.com>
2011-03-27 11:31:12 -0300

    Start the gmote server on initialization.

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -4,17 +4,10 @@ setxkbmap -option ctrl:nocaps
 
 [[ -s "$HOME/.wallpaper" ]] && feh --bg-scale $HOME/.wallpaper &> /dev/null &
 
-{
-  sleep 0.5
-  pypanel &
-  sleep 0.5
-  conky &
-  sleep 0.5
-  unclutter &
-  sleep 0.5
-  thunar --daemon &
-  sleep 0.5
-  dropboxd &
-  sleep 0.5
-  pytyle &
-} &> /dev/null &
+(sleep 0.5 && pypanel) &
+(sleep 0.5 && conky) &
+(sleep 0.5 && unclutter) &
+(sleep 0.5 && thunar --daemon) &
+(sleep 0.5 && dropboxd) &
+(sleep 0.5 && pytyle) &
+(sleep 0.5 && cd /opt/gmote/ && ./GmoteServer.sh) &
