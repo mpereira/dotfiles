68668488ebda3c849e7c0ab79cd650ea9f7a29ba
Murilo Pereira <murilo@murilopereira.com>
2011-05-22 18:59:04 -0300

    Different order for program starting.

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,9 +1,9 @@
 [[ -s "$HOME/.wallpaper" ]] && feh --bg-scale $HOME/.wallpaper &> /dev/null &
 
-(sleep 0.5 && pypanel) &
-(sleep 0.5 && conky) &
-(sleep 0.5 && unclutter) &
-(sleep 0.5 && thunar --daemon) &
-(sleep 0.5 && dropboxd) &
-(sleep 0.5 && pytyle) &
-(sleep 0.5 && cd /opt/gmote/ && ./GmoteServer.sh) &
+sleep 0.5 && (pypanel) &
+sleep 0.5 && (conky) &
+sleep 0.5 && (unclutter) &
+sleep 0.5 && (thunar --daemon) &
+sleep 0.5 && (pytyle) &
+sleep 0.5 && (dropboxd) &
+sleep 0.5 && (cd /opt/gmote/ && ./GmoteServer.sh) &
