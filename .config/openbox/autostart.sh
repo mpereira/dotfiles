b95640b9cb40e7a749c875f4c7223ba36a4c49e3
Murilo Pereira <murilo@murilopereira.com>
2011-06-27 15:33:59 -0300

    Different args for unclutter and using urxvtd.

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -2,8 +2,9 @@
 
 sleep 0.5 && (pypanel) &
 sleep 0.5 && (conky) &
-sleep 0.5 && (unclutter) &
+sleep 0.5 && (unclutter -i 3 -noevents) &
 sleep 0.5 && (thunar --daemon) &
 sleep 0.5 && (pytyle) &
 sleep 0.5 && (dropboxd) &
+sleep 0.5 && (urxvtd -q -o -f) &
 sleep 0.5 && (cd /opt/gmote/ && ./GmoteServer.sh) &
